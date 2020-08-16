054a4c3ce09a791780dd0746921d9e11371a1789
Tomas Maly <malytomas@ucpu.cz>
2020-08-16 23:03:22 +0200

    compatible with factorio 1.0 (forcefully skip the intro); armor is directly equiped; adds submachine gun with some ammo

--- control.lua
+++ control.lua
@@ -1,18 +1,21 @@
-script.on_event(defines.events.on_player_created, function(event)
-	local player = game.players[event.player_index]
 
+function initPlayer(player)
 	if player.character == nil then return end
+	if global == nil then
+		global = {}
+	end
+	if global.donePlayers == nil then
+		global.donePlayers = {}
+	end
+	if global.donePlayers[player] ~= nil then return end
+	global.donePlayers[player] = true
 
 	player.get_inventory(defines.inventory.character_main).clear()
+	player.get_inventory(defines.inventory.character_armor).clear()
 	player.get_inventory(defines.inventory.character_guns).clear()
 	player.get_inventory(defines.inventory.character_ammo).clear()
 
 	local items = {
-		-- equipment
-		{"power-armor", 1},
-		{"fusion-reactor-equipment", 1},
-		{"exoskeleton-equipment", 1},
-		{"personal-roboport-equipment", 1},
 		-- resources
 		{"coal", 150},
 		{"iron-plate", 500},
@@ -41,9 +44,39 @@ script.on_event(defines.events.on_player_created, function(event)
 		{"steam-engine", 20},
 		{"offshore-pump", 5},
 	}
-
-	for k,v in pairs(items) do
+	for _, v in pairs(items) do
 		player.insert{name = v[1], count = v[2]}
 	end
-end)
 
+	local armorInventory = player.get_inventory(defines.inventory.character_armor)
+	armorInventory.insert("power-armor")
+	local armorGrid = armorInventory.find_item_stack("power-armor").grid
+
+	local equipment = {
+		"fusion-reactor-equipment",
+		"exoskeleton-equipment",
+		"personal-roboport-equipment",
+	}
+	for _, v in pairs(equipment) do
+		armorGrid.put{name = v}
+	end
+
+	player.get_inventory(defines.inventory.character_guns).insert{name = "submachine-gun", count = 1}
+	player.get_inventory(defines.inventory.character_ammo).insert{name = "piercing-rounds-magazine", count = 50}
+end
+
+function onPlayerJoined(event)
+	local player = game.players[event.player_index]
+	initPlayer(player)
+end
+
+script.on_event({defines.events.on_player_joined_game, defines.events.on_player_created}, onPlayerJoined)
+
+function onModInit()
+	if remote.interfaces["freeplay"] then
+		remote.call("freeplay", "set_disable_crashsite", true)
+		remote.call("freeplay", "set_skip_intro", true)
+	end
+end
+
+script.on_init(onModInit)
